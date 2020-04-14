e1bb1a8c515a89897244fd0ce5ae498222a9667e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 15:22:41 +0000

    Deploy consiglionazionaledellericerche/sigla-main to github.com/consiglionazionaledellericerche/sigla-main.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.2.6',
+    VERSION: '6.2.7',
     LANGUAGE: 'it',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
