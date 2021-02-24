75c2b9fb962f35776a7c95aaf36955eaddcdd848
marco.spasiano <marco.spasiano@gmail.com>
2021-02-24 15:27:34 +0000

    Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.2.103',
+    VERSION: '6.3.0',
     LANGUAGE: 'it',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
